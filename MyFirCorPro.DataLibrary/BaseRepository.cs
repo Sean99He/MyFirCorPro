f0fef3a6f32cebe4c48853f1cbe3f41b41130a11
baidong.he <hebaidong1990@126.com>
2017-04-10 17:44:51 +0800

    add baserepository

--- MyFirCorPro.DataLibrary/BaseRepository.cs
+++ MyFirCorPro.DataLibrary/BaseRepository.cs
@@ -1,14 +1,30 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyFirCorPro.DataLibrary
 {
+    /// <summary>
+    /// 仓储基类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class BaseRepository<T> where T : class
     {
-        public BaseRepository()
+        private DbContext _dbContext;
+        public BaseRepository(DbContext dbContext)
         {
+            _dbContext = dbContext;
+        }
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="id">主键</param>
+        /// <returns>实体</returns>
+        public T Find(int id)
+        {
+            return _dbContext.Set<T>().Find(id);
         }
     }
 }
