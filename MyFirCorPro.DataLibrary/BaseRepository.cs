31017a172c12c83fb9dac24c30f74d8d95f8eb04
baidong.he <hebaidong1990@126.com>
2017-04-10 17:34:23 +0800

    add DataLibrary

---
+++ MyFirCorPro.DataLibrary/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirCorPro.DataLibrary
+{
+    public class BaseRepository<T> where T : class
+    {
+        public BaseRepository()
+        {
+        }
+    }
+}
