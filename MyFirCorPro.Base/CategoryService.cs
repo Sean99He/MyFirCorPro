5ad838667e322d8af15e94ba1c59092d50021640
baidong.he <hebaidong1990@126.com>
2017-04-11 10:09:35 +0800

    add categoryservice class

---
+++ MyFirCorPro.Base/CategoryService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirCorPro.DataLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirCorPro.Base
+{
+    /// <summary>
+    /// 栏目服务类
+    /// </summary>
+    public class CategoryService
+    {
+        private BaseRepository<Category> _baseRepository;
+        public CategoryService(DbContext dbContext)
+        {
+            _baseRepository = new BaseRepository<Category>(dbContext);
+        }
+        /// <summary>
+        /// 查找
+        /// </summary>
+        /// <param name="id">栏目Id</param>
+        /// <returns></returns>
+        public Category Find(int id)
+        {
+            return _baseRepository.Find(id);
+        }
+    }
+}
