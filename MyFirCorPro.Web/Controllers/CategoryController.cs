c66bf10f58d57d8512189f56726e1e49bfacf5fb
baidong.he <hebaidong1990@126.com>
2017-04-12 10:58:12 +0800

    实现数据访问

--- MyFirCorPro.Web/Controllers/CategoryController.cs
+++ MyFirCorPro.Web/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyFirCorPro.Base;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,21 @@ namespace MyFirCorPro.Web.Controllers
 {
     public class CategoryController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index(string id)
+        private MyFirCorProDbContext _dbContext;
+        private CategoryService _categoryService;
+        public CategoryController(MyFirCorProDbContext dbContext)
         {
-            return Content(id);
+            _dbContext = dbContext;
+            _categoryService = new CategoryService(dbContext);
+        }
+        // GET: /<controller>/
+        [Route("/Category/{id:int}")]
+        public IActionResult Index(int id)
+        {
+            var category = _categoryService.Find(id);
+            if (category == null)
+                return View("Error");
+            return View(category);
         }
     }
 }
